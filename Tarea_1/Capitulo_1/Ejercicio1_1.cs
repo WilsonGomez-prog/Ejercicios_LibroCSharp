ab9a1de09291535d698c31b71e45accb14b46717
wilscout20 <wilsonscout20@gmail.com>
2020-10-03 13:49:08 -0400

    Carpeta que contiene las clases con los ejercicios de la Tarea #1 (Capitulos 1, 2 y 3)

---
+++ Tarea_1/Capitulo_1/Ejercicio1_1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+class Ejercicio1_1
+{
+    public Ejercicio1_1()
+    {
+        Console.WriteLine("Wilson Alfonso Gomez Santiago.");
+    }
+}
---
+++ Tarea_1/Capitulo_2/Ejercicio2_4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Ejercicio2_4
+{
+    public Ejercicio2_4()
+    {
+        float centigrados, fahrenheit;
+        
+        Console.WriteLine("Bienvenido, este es un programa que convierte de grados centigrados a grados fahrenheit.");
+        
+        Console.Write("Ingrese la temperatura en grados centigrados que desea convertir: ");
+        centigrados = Convert.ToSingle(Console.ReadLine());
+
+        fahrenheit = (float)(centigrados * 1.8) + 32;
+        
+        Console.WriteLine("La temperatura ingresada({0}), en grados Fahrenheit es: {1}", centigrados, fahrenheit);
+    }
+}
+
