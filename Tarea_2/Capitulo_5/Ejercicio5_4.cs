aba7ba307c1774ebdbdfa81bb7c571b127851f2e
wilscout20 <wilsonscout20@gmail.com>
2020-10-03 13:49:52 -0400

    Carpeta que contiene las clases con los ejercicios de la Tarea #2 (Capitulos 4 y 5)

---
+++ Tarea_2/Capitulo_5/Ejercicio5_4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Ejercicio5_4
+{
+    private int numero;
+    private int factorial;
+
+    public Ejercicio5_4()
+    {
+        Init_class();
+    }
+    public int Calc_factorial(int factorial)
+    {
+        int resultado = 1;
+
+        for (int i = 1; i <= factorial; i++)
+        {
+            resultado*=i;
+        }
+
+        return resultado;
+    }
+
+    public void Init_class()
+    {
+        
+        Console.WriteLine("Bienvenido, este programa calcula el factorial de el numero que desee.");
+        Console.Write("Ingrese el numero que desee calcular: ");
+        numero = Convert.ToInt32(Console.ReadLine());
+        factorial = Calc_factorial(numero);
+        Console.WriteLine($"El factorial de {numero} es: {factorial}");
+    }
+}
